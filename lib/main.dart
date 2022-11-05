3552136a482e79b0c896da502c043f7e2e1653b4
Adedayo Agboola <kreativedayo@gmail.com>
2022-11-05 11:01:59 +0100

    Contact App Updated

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter Contact List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[900],
+        brightness: Brightness.dark,
+        primaryColor: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          headline1: TextStyle(fontFamily: 'Poppins',fontSize: 24, color: Colors.white),
+          headline6: TextStyle(fontFamily: 'Poppins',fontSize: 16, color: Colors.green),
+        ),
+      ),
+      home: MyHomePage(title: 'My Contact List'),
+    );
+  }
+}
